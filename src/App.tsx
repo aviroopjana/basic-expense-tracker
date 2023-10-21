53f7d16aeb72810a94c5c3080b89b57a082a2c0e
Aviroop Jana <aviroopjana@gmail.com>
2023-10-22 02:40:13 +0530

    feat: Add expense list ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import Form from './components/Form'
+import { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
+import Form from "./components/Form";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Grocery" },
+    { id: 2, description: "bbb", amount: 10, category: "Grocery" },
+    { id: 3, description: "ccc", amount: 10, category: "Grocery" },
+    { id: 4, description: "ddd", amount: 10, category: "Grocery" },
+  ]);
+
+  if (expenses.length === 0) return null;
 
   return (
     <div>
-      <Form onSubmit={(data)=> console.log(data)} />
+      <div className="mb-5">
+        <Form onSubmit={(data) => console.log(data)} />
+      </div>
+      <div>
+        <ExpenseList
+          expenses={expenses}
+          onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+        />
+      </div>      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

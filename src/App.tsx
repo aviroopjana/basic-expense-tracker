ab222af9a7857be9cf35847a338eebe586620088
Aviroop Jana <aviroopjana@gmail.com>
2023-10-22 04:08:24 +0530

    feat: Add expense list filter ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
 import Form from "./components/Form";
+import ExpenseFilter from "./components/ExpenseFilter";
 
 function App() {
+
+  const [selectedCategory, setSelectedCategory] = useState('');
+
   const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Grocery" },
-    { id: 2, description: "bbb", amount: 10, category: "Grocery" },
-    { id: 3, description: "ccc", amount: 10, category: "Grocery" },
-    { id: 4, description: "ddd", amount: 10, category: "Grocery" },
+    { id: 1, description: "aaa", amount: 10, category: "Groceries" },
+    { id: 2, description: "bbb", amount: 10, category: "Groceries" },
+    { id: 3, description: "ccc", amount: 10, category: "Education" },
+    { id: 4, description: "ddd", amount: 10, category: "Entertainment" },
   ]);
 
   if (expenses.length === 0) return null;
+
+  const visibleExpense =  selectedCategory ? expenses.filter(e=> e.category === selectedCategory) : expenses;
 
   return (
     <div>
@@ -18,8 +24,12 @@ function App() {
         <Form onSubmit={(data) => console.log(data)} />
       </div>
       <div>
+        <h2 className="fw-bold text-center mb-3">Expense list based on different categories</h2>
+        <div className="mb-3">
+          <ExpenseFilter onSelectCategory={(category)=> setSelectedCategory(category)} />
+        </div>
         <ExpenseList
-          expenses={expenses}
+          expenses={visibleExpense}
           onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
         />
       </div>      
